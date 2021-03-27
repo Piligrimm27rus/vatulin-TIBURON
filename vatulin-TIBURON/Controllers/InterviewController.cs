581cf19b8fdc6cdbcffc82d3569d987fe0eb2aa0
Женя Ватулин <cod40046@list.ru>
2021-03-27 22:53:39 +1000

    Добавьте файлы проекта.

---
+++ vatulin-TIBURON/Controllers/InterviewController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace vatulin_TIBURON.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterviewController : ControllerBase
+    {
+        string sql = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Testovoe-vatulin-tiburon;Integrated Security=True;";
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string query = "select Question.Question, Answer.Ans1, Answer.Ans2, Answer.Ans3 from Question, Answer " +
+                           $"where Question.Id = {id} and Question.AnswerId = Answer.Id";
+
+            string getResult = "";
+
+            using (SqlConnection con = new SqlConnection(sql))
+            {
+                con.Open();
+
+                getResult = DoCommand(query, con);
+            }
+
+            return getResult;
+        }
+
+        //{
+        //    "QuestionId": 4,
+        //    "Answer": "Answer",
+        //    "UserId": 0
+        //}
+        [HttpPost]
+        public string Post([FromBody] UserResult ur)
+        {
+            string query = $"insert into Result (QuestionId, Answer, UserId) values ({ur.QuestionId}, \'{ur.Answer}\', {ur.UserId})";
+            string query1 = $"select top(1) Id from Question where Id > {ur.QuestionId} " +
+                             $"and ServeyId = (select ServeyId from Question where Id = {ur.QuestionId})";
+
+            string postResult = "";
+
+            using (SqlConnection con = new SqlConnection(sql))
+            {
+                con.Open();
+
+                string commandResult = DoCommand(query, con);
+                postResult = DoCommand(query1, con);
+            }
+
+            return postResult;
+        }
+
+        private string DoCommand(string query, SqlConnection con)
+        {
+            DataTable table = new DataTable();
+            SqlDataReader dataReader;
+
+            using (SqlCommand myCommand = new SqlCommand(query, con))
+            {
+                dataReader = myCommand.ExecuteReader();
+                table.Load(dataReader);
+
+                dataReader.Close();
+            }
+
+            return JsonConvert.SerializeObject(table);
+        }
+    }
+}
---
+++ vatulin-TIBURON/Models/UserResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vatulin_TIBURON
+{
+    public class UserResult
+    {
+        public int UserId { get; set; }
+        public string Answer { get; set; }
+        public int QuestionId { get; set; }
+    }
+}
